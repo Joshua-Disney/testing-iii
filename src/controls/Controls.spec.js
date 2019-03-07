df06028d897d7598c729c71259f1457f67bcc60b
joshua-disney <joshua.a.disney@gmail.com>
2019-03-07 00:36:43 -0700

    Build tests for toggleClosed and toggleLocked

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,7 @@
 // Test away!
 import React from "react";
 import ReactDOM from "react-dom";
+import { render, fireEvent } from "react-testing-library";
 
 import Controls from "./Controls";
 
@@ -9,5 +10,27 @@ describe("<Controls />", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Controls />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("calls toggles lock function passed as props", () => {
+    const toggleLocked = jest.fn();
+
+    const { getByText } = render(
+      <Controls toggleLocked={toggleLocked} locked={false} closed={true} />
+    );
+
+    fireEvent.click(getByText(/lock gate/i));
+    expect(toggleLocked).toHaveBeenCalled();
+  });
+
+  it("calls toggles closed function passed as props", () => {
+    const toggleClosed = jest.fn();
+
+    const { getByText } = render(
+      <Controls toggleClosed={toggleClosed} locked={false} closed={false} />
+    );
+
+    fireEvent.click(getByText(/close gate/i || /open gate/i));
+    expect(toggleClosed).toHaveBeenCalled();
   });
 });
